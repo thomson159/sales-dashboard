85004a6f0712c6ee020476c2b2bf55378fd170dd
thomson159 <thomson159>
2026-01-08 22:49:50 +0100

    New store folder

--- app/store/ThemeContext.tsx
+++ app/store/ThemeContext.tsx
@@ -44,5 +44,6 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
 export const useTheme = (): ThemeContextType => {
   const context = useContext(ThemeContext);
   if (!context) throw new Error('useTheme must be used within a ThemeProvider');
+
   return context;
 };
