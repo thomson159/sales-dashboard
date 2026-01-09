93114d6325f1242ef205b69cb51f888b2585ba00
thomson159 <thomson159>
2026-01-10 00:56:31 +0100

    Fix table and table tests

--- app/components/Table/TableBody.tsx
+++ app/components/Table/TableBody.tsx
@@ -14,17 +14,17 @@ const TableBodyComponent = ({ data, visibleColumns }: TableBodyProps) => {
       {data.map((s, i) => (
         <tr key={i} className={`${i % 2 === 0 ? 'odd' : 'even'}`}>
           {visibleSet.has(index) && <td className={classes}>{i + 1}</td>}
-          {visibleSet.has('date') && <td className={classes}>{formatDate(s.date) || ''}</td>}
+          {visibleSet.has('date') && <td className={classes}>{formatDate(s.date) || '-'}</td>}
           {visibleSet.has('channel_name') && (
             <td className={classes}>{normalizeChannelName(s.channel_name) || ''}</td>
           )}
           {visibleSet.has('order_status_id') && (
-            <td className={classes}>{s.order_status_id || ''}</td>
+            <td className={classes}>{s.order_status_id || '-'}</td>
           )}
           {visibleSet.has('sum_sales') && (
-            <td className={classes}>{formatNumber(s.sum_sales) || ''}</td>
+            <td className={classes}>{formatNumber(s.sum_sales) || '-'}</td>
           )}
-          {visibleSet.has('count_orders') && <td className={classes}>{s.count_orders || ''}</td>}
+          {visibleSet.has('count_orders') && <td className={classes}>{s.count_orders || '-'}</td>}
         </tr>
       ))}
     </tbody>
