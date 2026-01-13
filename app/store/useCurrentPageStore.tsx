db00d8d9b3a4a40e1c24a8720de965075a543730
thomson159 <thomson159>
2026-01-13 05:41:26 +0100

    Add persist

--- app/store/useCurrentPageStore.tsx
+++ app/store/useCurrentPageStore.tsx
@@ -1,11 +1,19 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export interface CurrentPageState {
   currentPage: number;
   setCurrentPage: (currentPage: number) => void;
 }
 
-export const useCurrentPageStore = create<CurrentPageState>()((set) => ({
-  currentPage: 1,
-  setCurrentPage: (currentPage) => set({ currentPage }),
-}));
+export const useCurrentPageStore = create<CurrentPageState>()(
+  persist(
+    (set) => ({
+      currentPage: 1,
+      setCurrentPage: (currentPage) => set({ currentPage }),
+    }),
+    {
+      name: 'dashboard-current-page',
+    },
+  ),
+);
--- app/store/usePageSizeStore.tsx
+++ app/store/usePageSizeStore.tsx
@@ -1,11 +1,19 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export interface PageSizeState {
   pageSize: number;
   setPageSize: (pageSize: number) => void;
 }
 
-export const usePageSizeStore = create<PageSizeState>()((set) => ({
-  pageSize: 40,
-  setPageSize: (pageSize) => set({ pageSize }),
-}));
+export const usePageSizeStore = create<PageSizeState>()(
+  persist(
+    (set) => ({
+      pageSize: 40,
+      setPageSize: (pageSize) => set({ pageSize }),
+    }),
+    {
+      name: 'dashboard-page-size',
+    },
+  ),
+);
